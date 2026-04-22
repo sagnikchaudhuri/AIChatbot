2b5328aa36d6a01907f3d78d1ded4e9722167719
sagnikchaudhuri <sagnik.chaudhuri15@gmail.com>
2026-04-22 11:29:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from routes.whatsapp_webhook import whatsapp_bp
+
+app = Flask(__name__)
+
+app.register_blueprint(whatsapp_bp)
+
+@app.route("/")
+def home():
+    return "WhatsApp AI Bot Running"
+
+if __name__ == "__main__":
+    app.run()
